e7df63290c18800e4f33accbd9ab4576060c3ada
Connor Turland <connorturland@gmail.com>
2022-02-04 20:47:15 -0800

    fmt

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { App } from 'electron'
-import {HolochainRunnerOptions, PathOptions} from './options'
+import { HolochainRunnerOptions, PathOptions } from './options'
 import {
   runHolochain,
   StateSignal,
@@ -7,13 +7,22 @@ import {
   STATUS_EVENT,
   APP_PORT_EVENT,
 } from './holochain'
-import {defaultHolochainRunnerBinaryPath, defaultLairKeystoreBinaryPath} from "./binaries";
-import * as childProcess from "child_process";
-import * as fs from "fs";
-import * as kill from 'tree-kill';
+import {
+  defaultHolochainRunnerBinaryPath,
+  defaultLairKeystoreBinaryPath,
+} from './binaries'
+import * as childProcess from 'child_process'
+import * as fs from 'fs'
+import * as kill from 'tree-kill'
 
-export { StateSignal, StatusUpdates, HolochainRunnerOptions, STATUS_EVENT, PathOptions, APP_PORT_EVENT }
-
+export {
+  StateSignal,
+  StatusUpdates,
+  HolochainRunnerOptions,
+  STATUS_EVENT,
+  PathOptions,
+  APP_PORT_EVENT,
+}
 
 // start up lair and holochain-runner processes,
 // automatically shut them down on app quit,
@@ -22,7 +31,7 @@ export default async function initAgent(
   app: App,
   opts: HolochainRunnerOptions,
   binaryPaths?: PathOptions
-): Promise<{ statusEmitter: StatusUpdates, shutdown: () => Promise<void> }> {
+): Promise<{ statusEmitter: StatusUpdates; shutdown: () => Promise<void> }> {
   // wait for the app to be ready
   await app.whenReady()
   const statusEmitter = new StatusUpdates()
@@ -32,81 +41,79 @@ export default async function initAgent(
   let lairHandle: childProcess.ChildProcessWithoutNullStreams
   let holochainHandle: childProcess.ChildProcessWithoutNullStreams
   ;(async () => {
-      [lairHandle, holochainHandle] = await runHolochain(
-        statusEmitter,
-        opts,
-        binaryPaths
-      )
-      app.on('will-quit', async () => {
-        // SIGTERM signal is the default, and that's good
-        await killHolochain(lairHandle, holochainHandle)
-      })
+    ;[lairHandle, holochainHandle] = await runHolochain(
+      statusEmitter,
+      opts,
+      binaryPaths
+    )
+    app.on('will-quit', async () => {
+      // SIGTERM signal is the default, and that's good
+      await killHolochain(lairHandle, holochainHandle)
+    })
   })()
   return {
     statusEmitter,
     shutdown: async () => {
       // SIGTERM signal is the default, and that's good
       await killHolochain(lairHandle, holochainHandle)
-    }
+    },
   }
 }
-
 
 /**
  *
  */
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
-
 
 /**
  * Kill handles and their children
  */
-async function killHolochain(lairHandle: childProcess.ChildProcessWithoutNullStreams,
-                             holochainHandle: childProcess.ChildProcessWithoutNullStreams) {
+async function killHolochain(
+  lairHandle: childProcess.ChildProcessWithoutNullStreams,
+  holochainHandle: childProcess.ChildProcessWithoutNullStreams
+) {
   // Kill holochain and its children
-  let canWaitForHolochain = false;
-  if(holochainHandle && holochainHandle.pid) {
-    canWaitForHolochain = true;
-    console.debug('Killing holochain sub processes...');
-    kill(holochainHandle.pid, function(err) {
-      canWaitForHolochain = false;
-      if(!err) {
-        console.debug('killed all holochain sub processes');
+  let canWaitForHolochain = false
+  if (holochainHandle && holochainHandle.pid) {
+    canWaitForHolochain = true
+    console.debug('Killing holochain sub processes...')
+    kill(holochainHandle.pid, function (err) {
+      canWaitForHolochain = false
+      if (!err) {
+        console.debug('killed all holochain sub processes')
       } else {
         console.error(err)
       }
-    });
+    })
   }
   // Kill lair-keystore and its children
-  let canWaitForKeystore = false;
-  if(lairHandle && lairHandle.pid) {
-    canWaitForKeystore = true;
-    console.debug('Killing lair-keystore sub processes...');
-    kill(lairHandle.pid, function(err) {
-      canWaitForKeystore = false;
-      if(!err) {
-        console.debug('killed all lair-keystore sub processes');
+  let canWaitForKeystore = false
+  if (lairHandle && lairHandle.pid) {
+    canWaitForKeystore = true
+    console.debug('Killing lair-keystore sub processes...')
+    kill(lairHandle.pid, function (err) {
+      canWaitForKeystore = false
+      if (!err) {
+        console.debug('killed all lair-keystore sub processes')
       } else {
         console.error(err)
       }
-    });
+    })
   }
   // Wait for the kill commands to complete and exit anyway after a timeout
-  console.debug('waiting...');
+  console.debug('waiting...')
   const start_time = Date.now()
-  while(canWaitForHolochain || canWaitForKeystore) {
-    await sleep(10);
+  while (canWaitForHolochain || canWaitForKeystore) {
+    await sleep(10)
     if (Date.now() - start_time > 5 * 1000) {
-      console.error('Killing sub-processes TIMED-OUT. Aborted.');
-      break;
+      console.error('Killing sub-processes TIMED-OUT. Aborted.')
+      break
     }
   }
-  console.debug('Killing sub-processes DONE.');
+  console.debug('Killing sub-processes DONE.')
 }
-
-
 
 /**
  *
@@ -117,23 +124,25 @@ export function getRunnerVersion(runnerBinaryPath?: string): string {
     : defaultHolochainRunnerBinaryPath
 
   if (!fs.existsSync(holochainRunnerBinaryPath)) {
-    console.error("holochain-runner binary not found at path: " + holochainRunnerBinaryPath)
-    return "holochain-runner missing"
+    console.error(
+      'holochain-runner binary not found at path: ' + holochainRunnerBinaryPath
+    )
+    return 'holochain-runner missing'
   }
 
-  const holochainHandle = childProcess.spawnSync(
-    holochainRunnerBinaryPath,
-    ['--version']
-  )
+  const holochainHandle = childProcess.spawnSync(holochainRunnerBinaryPath, [
+    '--version',
+  ])
 
   if (holochainHandle.error) {
-    console.error("Calling holochain-runner failed: " + holochainHandle.error.message)
-    return "holochain-runner broken"
+    console.error(
+      'Calling holochain-runner failed: ' + holochainHandle.error.message
+    )
+    return 'holochain-runner broken'
   }
 
   return holochainHandle.stdout.toString()
 }
-
 
 /**
  *
@@ -144,18 +153,15 @@ export function getLairVersion(lairBinaryPath?: string): string {
     : defaultLairKeystoreBinaryPath
 
   if (!fs.existsSync(binaryPath)) {
-    console.error("lair-keystore binary not found at path: " + binaryPath)
-    return "lair-keystore missing"
+    console.error('lair-keystore binary not found at path: ' + binaryPath)
+    return 'lair-keystore missing'
   }
 
-  const handle = childProcess.spawnSync(
-    binaryPath,
-    ['--version']
-  )
+  const handle = childProcess.spawnSync(binaryPath, ['--version'])
 
   if (handle.error) {
-    console.error("Calling lair-keystore failed: " + handle.error.message)
-    return "lair-keystore broken"
+    console.error('Calling lair-keystore failed: ' + handle.error.message)
+    return 'lair-keystore broken'
   }
 
   return handle.stdout.toString()
